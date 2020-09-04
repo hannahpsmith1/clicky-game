9dc189eacac84e60008a41fe7339f9b2a5eee965
Hannah Smith <hannahpsmith1@gmail.com>
2020-09-03 18:25:05 -0700

    Updates

---
+++ precache-manifest.532a5f88ec32778dfdef291830198a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac8ecfe64cd745b64f0b4088cf2ded7",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "be7b35990f54d0147d9d",
+    "url": "/clicky-game/static/css/main.5f118921.chunk.css"
+  },
+  {
+    "revision": "ee68d53e3810843daeb1",
+    "url": "/clicky-game/static/js/2.b1f20aee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.b1f20aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7b35990f54d0147d9d",
+    "url": "/clicky-game/static/js/main.f5f23bd5.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
